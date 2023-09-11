3936e3aa631b3923bcc594ee5347ecc400f18acf
PluggeRo <r.plugge@unitybox.de>
2023-09-11 15:47:21 +0200

    feat: Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# CrawlMeMaybe
-Asynchronous Web Crawler in Python
+## CrawlMeMaybe
+
+### Overview
+
+This is an asynchronous web crawler built in Python, using the aiohttp and BeautifulSoup4 libraries. The crawler fetches URLs from a given website up to a specified depth, and has options to include or exclude external links.
+
+The crawler is designed to be fast and efficient, capable of fetching multiple URLs concurrently. It saves the fetched URLs in a text file, sorted in alphabetical order.
+Features
+
+- Asynchronous crawling using aiohttp
+- URL parsing using BeautifulSoup4
+- Fetches URLs up to a given depth
+- Option to include or exclude external links
+- Saves crawled URLs in alphabetical order to a text file
+
+### Requirements
+
+- Python 3.6+
+- aiohttp
+- BeautifulSoup4
+
+### Installation
+
+1. First, clone the repository:
+``` bash
+git clone https://github.com/PluggeRo/CrawlMeMaybe.git
+```
+2. Activate virtual env:
+``` bash
+cd CrawlMeMaybe
+python -m venv venv
+source venv/bin/activate
+```
+3. Then install the required packages:
+``` bash
+pip install -r requirements.txt
+```
+
+### Usage
+
+``` bash
+python crawlmemaybe.py -h
+```
+
+Run the crawler with the following command-line arguments:
+- --url : The starting URL (required)
+- --output : Output file to save the URLs (required)
+- --depth : Maximum depth to search (required)
+- --include-external : Flag to include external links (optional)
+
+``` bash
+python crawlmemaybe.py --url https://example.com --output urls.txt --depth 3 --include_external
+```
+
+>This will crawl https://example.com up to a depth of 3, including external links, and save the URLs to urls.txt.
+
+### Contributing
+
+If you'd like to contribute, please fork the repository and make changes as you'd like. Pull requests are warmly welcome.
+
+### License
+
+MIT
+
+### Disclaimer
+
+Please make sure to respect website terms, robots.txt, and any rate limitations when running the crawler.
---
+++ crawlmemaybe.py
@@ -0,0 +1,95 @@
+import aiohttp
+import asyncio
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
+import argparse
+from asyncio import Queue
+
+# Worker coroutine for crawling URLs up to a specified depth
+async def worker(session, queue, visited, lock, sorted_output, max_depth, include_external, root_domain):
+    while True:
+        # Dequeue a URL and its depth from the central task queue
+        depth, url = await queue.get()
+
+        # Skip if depth exceeds maximum
+        if depth > max_depth:
+            queue.task_done()
+            continue
+
+        # Check if the URL is an external link
+        domain = urlparse(url).netloc
+        if not include_external and domain != root_domain:
+            queue.task_done()
+            continue
+
+        # Atomically check and update the 'visited' set
+        async with lock:
+            if url in visited:
+                queue.task_done()
+                continue
+            visited.add(url)
+
+        # Fetch the URL content
+        try:
+            async with session.get(url) as response:
+                text = await response.text()
+        except:
+            print(f"Failed to get URL {url}")
+            queue.task_done()
+            continue
+
+        # Append the URL to the sorted_output list and print it
+        sorted_output.append(url)
+        print(url)
+
+        # Parse the page content and enqueue new URLs
+        soup = BeautifulSoup(text, 'html.parser')
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if href:
+                full_url = urljoin(url, href)
+                await queue.put((depth + 1, full_url))
+
+        # Mark the task as done
+        queue.task_done()
+
+# Main coroutine for managing the crawler
+async def main(args):
+    visited = set()
+    lock = asyncio.Lock()
+    queue = Queue()
+    await queue.put((0, args.url))
+
+    # Extract the root domain of the starting URL
+    root_domain = urlparse(args.url).netloc
+
+    # Create an HTTP session and an empty list to hold the URLs
+    sorted_output = []
+    async with aiohttp.ClientSession() as session:
+        # Launch worker tasks
+        workers = [asyncio.create_task(worker(session, queue, visited, lock, sorted_output, args.depth, args.include_external, root_domain)) for _ in range(10)]
+        # Wait for all URLs to be processed
+        await queue.join()
+
+        # Cancel the workers
+        for w in workers:
+            w.cancel()
+
+    # Sort and write the URLs to the output file
+    sorted_output.sort()
+    with open(args.output, 'w') as output_file:
+        for url in sorted_output:
+            output_file.write(url + "\n")
+
+# Entry point of the script
+if __name__ == "__main__":
+    # Command-line argument parsing
+    parser = argparse.ArgumentParser(description='Asynchronous Web Crawler')
+    parser.add_argument('--url', type=str, required=True, help='Starting URL')
+    parser.add_argument('--output', type=str, required=True, help='Output file')
+    parser.add_argument('--depth', type=int, default=1, help='Maximum depth to search')
+    parser.add_argument('--include-external', action='store_true', help='Whether to include external links')
+    args = parser.parse_args()
+
+    # Run the main coroutine
+    asyncio.run(main(args))
